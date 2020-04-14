ff78038b1750cb7781b4a0a98f71ecc31627d7fc
magzhan <maksum.magzhan0224@gmail.com>
2020-04-15 03:31:40 +0600

    feat(*): create list of movies

---
+++ src/app/list.js
@@ -0,0 +1,377 @@
+export const list = [
+  {
+    title: "First",
+    movies: [
+      {
+        title: "The Shawshank Redemption",
+        id: "tt0111161",
+      },
+      {
+        title: "The Godfather",
+        id: "tt0068646",
+      },
+      {
+        title: "The Godfather: Part II",
+        id: "tt0071562",
+      },
+      {
+        title: "Pulp Fiction",
+        id: "tt0110912",
+      },
+      {
+        title: "The Good, the Bad and the Ugly",
+        id: "tt0060196",
+      },
+      {
+        title: "The Dark Knight",
+        id: "tt0468569",
+      },
+    ],
+    id: 1,
+  },
+  {
+    title: "Second",
+    movies: [
+      {
+        title: "12 Angry Men",
+        id: "tt0050083",
+      },
+      {
+        title: "Schindler's List",
+        id: "tt0108052",
+      },
+      {
+        title: "The Lord of the Rings: The Return of the King",
+        id: "tt0167260",
+      },
+      {
+        title: "Fight Club",
+        id: "tt0137523",
+      },
+      {
+        title: "Star Wars: Episode V - The Empire Strikes Back",
+        id: "tt0080684",
+      },
+      {
+        title: "The Lord of the Rings: The Fellowship of the Ring",
+        id: "tt0120737",
+      },
+      {
+        title: "One Flew Over the Cuckoo's Nest",
+        id: "tt0073486",
+      },
+      {
+        title: "Inception",
+        id: "tt1375666",
+      },
+      {
+        title: "Goodfellas",
+        id: "tt0099685",
+      },
+      {
+        title: "Star Wars",
+        id: "tt0076759",
+      },
+      {
+        title: "Seven Samurai",
+        id: "tt0047478",
+      },
+    ],
+    id: 2,
+  },
+  {
+    title: "Third",
+    movies: [
+      {
+        title: "Forrest Gump",
+        id: "tt0109830",
+      },
+      {
+        title: "The Matrix",
+        id: "tt0133093",
+      },
+      {
+        title: "The Lord of the Rings: The Two Towers",
+        id: "tt0167261",
+      },
+      {
+        title: "City of God",
+        id: "tt0317248",
+      },
+      {
+        title: "Se7en",
+        id: "tt0114369",
+      },
+      {
+        title: "The Silence of the Lambs",
+        id: "tt0102926",
+      },
+      {
+        title: "Once Upon a Time in the West",
+        id: "tt0064116",
+      },
+      {
+        title: "Casablanca",
+        id: "tt0034583",
+      },
+      {
+        title: "The Usual Suspects",
+        id: "tt0114814",
+      },
+      {
+        title: "Raiders of the Lost Ark",
+        id: "tt0082971",
+      },
+      {
+        title: "Rear Window",
+        id: "tt0047396",
+      },
+    ],
+    id: 3,
+  },
+  {
+    title: "Fourth",
+    movies: [
+      {
+        title: "It's a Wonderful Life",
+        id: "tt0038650",
+      },
+      {
+        title: "Psycho",
+        id: "tt0054215",
+      },
+      {
+        title: "Léon: The Professional",
+        id: "tt0110413",
+      },
+      {
+        title: "Sunset Blvd.",
+        id: "tt0043014",
+      },
+      {
+        title: "American History X",
+        id: "tt0120586",
+      },
+      {
+        title: "Apocalypse Now",
+        id: "tt0078788",
+      },
+      {
+        title: "Terminator 2: Judgment Day",
+        id: "tt0103064",
+      },
+      {
+        title: "Saving Private Ryan",
+        id: "tt0120815",
+      },
+      {
+        title: "Memento",
+        id: "tt0209144",
+      },
+      {
+        title: "City Lights",
+        id: "tt0021749",
+      },
+      {
+        title:
+          "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
+        id: "tt0057012",
+      },
+      {
+        title: "Alien",
+        id: "tt0078748",
+      },
+      {
+        title: "Modern Times",
+        id: "tt0027977",
+      },
+      {
+        title: "Spirited Away",
+        id: "tt0245429",
+      },
+    ],
+    id: 4,
+  },
+  {
+    title: "Fifth",
+    movies: [
+      {
+        title: "North by Northwest",
+        id: "tt0053125",
+      },
+      {
+        title: "Back to the Future",
+        id: "tt0088763",
+      },
+      {
+        title: "Life Is Beautiful",
+        id: "tt0118799",
+      },
+      {
+        title: "The Shining",
+        id: "tt0081505",
+      },
+      {
+        title: "The Pianist",
+        id: "tt0253474",
+      },
+      {
+        title: "Citizen Kane",
+        id: "tt0033467",
+      },
+    ],
+    id: 5,
+  },
+  {
+    title: "Sixth",
+    movies: [
+      {
+        title: "The Departed",
+        id: "tt0407887",
+      },
+      {
+        title: "M",
+        id: "tt0022100",
+      },
+      {
+        title: "Paths of Glory",
+        id: "tt0050825",
+      },
+      {
+        title: "Vertigo",
+        id: "tt0052357",
+      },
+      {
+        title: "Django Unchained",
+        id: "tt1853728",
+      },
+      {
+        title: "Double Indemnity",
+        id: "tt0036775",
+      },
+      {
+        title: "The Dark Knight Rises",
+        id: "tt1345836",
+      },
+      {
+        title: "Aliens",
+        id: "tt0090605",
+      },
+      {
+        title: "Taxi Driver",
+        id: "tt0075314",
+      },
+      {
+        title: "American Beauty",
+        id: "tt0169547",
+      },
+      {
+        title: "The Green Mile",
+        id: "tt0120689",
+      },
+      {
+        title: "Gladiator",
+        id: "tt0172495",
+      },
+      {
+        title: "The Intouchables",
+        id: "tt1675434",
+      },
+    ],
+    id: 6,
+  },
+  {
+    title: "Seventh",
+    movies: [
+      {
+        title: "WALL·E",
+        id: "tt0910970",
+      },
+      {
+        title: "The Lives of Others",
+        id: "tt0405094",
+      },
+      {
+        title: "Toy Story 3",
+        id: "tt0435761",
+      },
+      {
+        title: "The Great Dictator",
+        id: "tt0032553",
+      },
+      {
+        title: "The Prestige",
+        id: "tt0482571",
+      },
+      {
+        title: "A Clockwork Orange",
+        id: "tt0066921",
+      },
+      {
+        title: "Lawrence of Arabia",
+        id: "tt0056172",
+      },
+      {
+        title: "Amélie",
+        id: "tt0211915",
+      },
+      {
+        title: "To Kill a Mockingbird",
+        id: "tt0056592",
+      },
+      {
+        title: "Reservoir Dogs",
+        id: "tt0105236",
+      },
+      {
+        title: "Das Boot",
+        id: "tt0082096",
+      },
+      {
+        title: "The Lion King",
+        id: "tt0110357",
+      },
+      {
+        title: "Cinema Paradiso",
+        id: "tt0095765",
+      },
+      {
+        title: "Star Wars: Episode VI - Return of the Jedi",
+        id: "tt0086190",
+      },
+      {
+        title: "The Treasure of the Sierra Madre",
+        id: "tt0040897",
+      },
+      {
+        title: "The Third Man",
+        id: "tt0041959",
+      },
+      {
+        title: "Once Upon a Time in America",
+        id: "tt0087843",
+      },
+      {
+        title: "Requiem for a Dream",
+        id: "tt0180093",
+      },
+      {
+        title: "Eternal Sunshine of the Spotless Mind",
+        id: "tt0338013",
+      },
+      {
+        title: "Full Metal Jacket",
+        id: "tt0093058",
+      },
+      {
+        title: "Oldboy",
+        id: "tt0364569",
+      },
+      {
+        title: "Braveheart",
+        id: "tt0112573",
+      },
+    ],
+    id: 7,
+  },
+];
